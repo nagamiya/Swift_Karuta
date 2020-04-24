d54405ea887f44799723c9d1d916750aaed8b1a8
nagamiya <appngmy@gmail.com>
2020-04-24 23:51:31 +0900

    Initial Commit

---
+++ Swift_Karuta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_Karuta
+//
+//  Created by nagamiya on 2020/04/24.
+//  Copyright © 2020 nagamiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
